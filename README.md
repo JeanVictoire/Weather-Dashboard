49cbb0024964a84253bb14b0cf0f5abbbb49d024
JeanVictoire <jeanthecoders@gmail.com>
2022-05-16 15:27:06 -0400

    Weather-API-App

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Weather-Dashboard
+
 A weather dashboard that run in the browser.
+This is a weather dashboard that displays the current forecast for the users searched location, as well as a five day forecast for the location.
+The UV Index has a background color that will change depending on the current UV index, with green being safest, and red being dangerous.
+
+# Usage
+
+Enter the desired location in the search bar, and hit SEARCH!. This will get the current forecast and a five day forecast of the location.
+
+Link to Webpage:
+
+# Built With
+
+[HTML]
+[CSS]
+[JS]
+[Jquery]
+[Bootstrap]
+
+# Features
+
+Open Weather API
+Current Forecast
+UV Index with color changing background
+Five Day Forecast
+
+# Credits
+
+Open Weather API [https://openweathermap.org/api]
+w3schools : [https://www.w3schools.com/]
+Stack Overflow : [https://stackoverflow.com/]
+MDN Documentation : [https://developer.mozilla.org/en-US/]
+
+# License
+
+MIT LIcense
+
+# Contact info
+
+Name: Jean Victorie
+Email: JeanTheCoders@gmail.com
+GIT: https://github.com/JeanVictoire
+Twitter: @JeanTheCoders
---
+++ assets/script.js
@@ -0,0 +1,109 @@
+var generateHistory = function() {
+    // get localStorage information
+    var cityHistory = JSON.parse(localStorage.getItem("inputCity"));
+    if (!$(".search-history-container")?.length && cityHistory?.length) {
+        $(".city-column").append('<div class="search-history-container pt-4 border-top border-dark"></div>');
+    }
+    // clear any info on the container in case of old searches
+    $(".search-history-container").html("");
+    for (let cityCounter = 0;cityCounter < cityHistory?.length;cityCounter++) {
+        let city = cityHistory[cityCounter];
+        $(".search-history-container").append(`<button id="CityBtn${cityCounter}" class="btn btn-dark btn-block">${city}</button>`);
+        $(".search-history-container").on("click",`#CityBtn${cityCounter}`, function () {
+            createQuery(city);
+            console.log(`you clicked ${cityCounter}`)
+            localStorage.setItem("city", JSON.stringify(city));
+        });
+    }
+}
+
+var loadHistory = function() {
+    var lastSearched = JSON.parse(localStorage.getItem("inputCity"));
+    if (lastSearched?.length > 0) {
+    let lastCity = lastSearched[lastSearched.length - 1];
+    //display last searched city's weather onload
+    console.log(lastCity);
+    createQuery(lastCity);
+    generateHistory();
+    }
+}
+
+var createQuery = function(city) {
+    let inputCity = city ? city : $("#citySearch").val();
+    var firstQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&units=metric&appid=ae091cae15863695a3bd2a2f28f74012";
+
+    $.ajax({
+        url: firstQuery,
+        method: "GET",
+    }).then(function(data) {
+        var secondQuery = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&units=metric&appid=ae091cae15863695a3bd2a2f28f74012";
+        $.ajax ({
+            url: secondQuery,
+            method: "GET",
+        }).then(function (uvExtendedData) {
+            var weatherIcon = uvExtendedData.current.weather[0].icon;
+            var iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+
+            $(".forecast-column").html("").append('<div class="jumbotron bg-light border border-dark rounded p-3 mt-2 todays-forecast"></div>');
+            $(".todays-forecast").append(`<h2 class="current-city">${data.name} <span class="current-city-date">${moment.unix(uvExtendedData?.current?.dt).format("M/DD/YYYY")}</span> <img id="weather-icon" src="${iconUrl}"/></h2>`);
+            $(".todays-forecast").append(`<p class="current-temp">Temperature: ${uvExtendedData.current.temp + " &deg;C"}</p>`);
+            $(".todays-forecast").append(`<p class="city-humidity">Humidity: ${uvExtendedData.current.humidity + " %"}</p>`);
+            $(".todays-forecast").append(`<p class="city-wind">Wind Speed: ${uvExtendedData.current.wind_speed + " MPH"}</p>`);
+            $(".todays-forecast").append(`<p>UV Index: <span class="${uivClassName(uvExtendedData.current.uvi)}">${uvExtendedData.current.uvi}</span></p>`);
+
+            // append divs to container
+            $(".forecast-column").append('<div class="future-forecast"></div>');
+            $(".future-forecast").append("<h2>5-Day Forecast:</h2>");
+            $(".future-forecast").append('<div class="card-deck"></div>');
+
+            // display current queried cities. date, weather icon, temperature, and humidity
+            uvExtendedData?.daily?.map((day, index) => {
+                if (index > 0 && index < 6) {
+                    $(".card-deck").append(
+                        `<div class="card bg-success px-2 text-white" id="${'card' + index}">
+                        <h7 class="card-title">${moment.unix(day.dt).format("M/DD/YYYY")}</h7>
+                        <h6 class="card-subtitle"><img id="weatherIcon" src="https://openweathermap.org/img/wn/${day.weather[0].icon}.png"/></h6>
+                        <p class="card-text p-0">Temp: ${day.temp.day + " &deg;C"}</p>
+                        <h7 class="card-text">Humidity: ${day.humidity + "%"}</h7>
+                        </div>`);
+                }
+            });
+        })
+    })
+}
+
+var uivClassName = function (uvi) {
+    if (uvi < 4) {
+        return "uv-favorable";
+    } else if (uvi >= 4 && uvi <= 10) {
+        return "uv-moderate";
+    } else if (uvi > 11) {
+        return "uv-extreme";
+    } else {
+        return "uv-undefined";
+    }
+}
+
+var haceclick = function() {
+    console.log("click");
+}
+
+$("#btn-search").on("click", function (event) {
+        event.preventDefault();
+        let inputCity = $("#city-search").val();
+        if (inputCity === "" || inputCity === null){
+            alert("Invalid Input");
+        }
+        else {
+            createQuery(inputCity);
+            // get list of cities from local storage and if data doesn't exist, then create an empty array
+            var cityArray = JSON.parse(localStorage.getItem("inputCity")) || [];
+            // add inputCity to list
+            cityArray.push(inputCity);
+            // re-save list of cities TO local storage
+            localStorage.setItem("inputCity", JSON.stringify(cityArray));
+            generateHistory(cityArray);
+        }
+});
+
+loadHistory();
